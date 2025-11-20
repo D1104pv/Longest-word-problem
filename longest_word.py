dc66abc7d901990d76cd97d41d67ab8f087da84a
Deepanshu Yadav <d1104pv@gmail.com>
2025-11-20 17:05:24 +0530

    added sentence validation
    Added input validation to check if the input is a string.

--- longest_word.py
+++ longest_word.py
@@ -1,19 +1,17 @@
 def longest_word(sentence):
+    if not isinstance(sentence, str):
+        return "Please enter a sentence"
+    
     word_list = sentence.split()
     special_characters = (" !@#$%^&*(){}[]?/<>,._-\|~`:;=+'")
     onlywords = []
-    
     for i in word_list:
         onlywords.append(i.strip(special_characters))
     
     only_characters = []
     for i in onlywords:
         only_characters.append(i.replace("'", ""))
-    
-    print(only_characters)
-    
-    
-    
+        
     longestword = ""
     longest_word_length = 0
     for i in only_characters:
@@ -22,4 +20,5 @@ def longest_word(sentence):
     for i in only_characters:
         if len(i) == longest_word_length:
             longestword = i
+            
             return longestword
